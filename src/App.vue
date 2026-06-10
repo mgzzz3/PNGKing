05d48bd7e0ba7a62966dc30a53ba2a64078b2b34
mgzzz3 <zgm_ios@163.com>
2026-06-10 10:52:28 +0800

    Remove GitHub references from site

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,6 @@ import AppHeader from '@/components/AppHeader.vue'
     </main>
     <footer class="site-footer">
       <span>PNGKing · 图片只在你的浏览器里处理</span>
-      <a href="https://github.com" target="_blank" rel="noreferrer">开源项目</a>
     </footer>
   </div>
 </template>
